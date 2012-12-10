8c323a1056a23d5cba3a07f0210a0a694f6823e4
Nick <nick-b@live.nl>
2012-12-10 10:14:07 +0100

    Jquery togevoegt. datepicker gemaakt

--- index.php
+++ index.php
@@ -3,7 +3,11 @@
 	<head>
 		<title>Jumbo games</title>
 		
-		<link rel="stylesheet" type="text/css" href="./css/opmaak.css" />
+		<link rel='stylesheet' type='text/css' href='./css/opmaak.css' />
+		<link rel='stylesheet' type='text/css' href='./css/overcast/jquery-ui-1.9.2.custom.css' />
+		<script style='text/javascript' src='./js/script.js'></script>
+		<script style='text/javascript' src='./Jquery/jquery-1.8.3.js'></script>
+		<script style='text/javascript' src='./Jquery/jquery-ui-1.9.2.custom.js'></script>
 	</head>
 
 	<body>
--- opdracht.php
+++ opdracht.php
@@ -1,6 +1,14 @@
+<script type='text/javascript'>
+$(function() {
+        $( "#datepicker" ).datepicker();
+		$( "#datepicker" ).datepicker("option", "dateFormat","DD, d MM, yy");
+    });
+</script>
 <form action='' method=''>
-	Plaats hier een korte omschrijving van uw opdracht
+	Plaats hier een korte omschrijving van uw opdracht.
 	<textarea cols='80' rows='5' name='Kort'></textarea><br>
-	Plaats hier een uigebrijde omschrijving van uw opdracht
-	<textarea cols='80' rows='10' name='Lang'></textarea>
+	Plaats hier een uigebrijde omschrijving van uw opdracht.
+	<textarea cols='80' rows='10' name='Lang'></textarea><br>
+	geef hier de opleveringsdatum.<br>
+	<input type='text' name='datepicker' id='datepicker'/>
 </form>
